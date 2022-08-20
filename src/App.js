99e0d07593d80ddd9746aab5dfcf78e678e4cf7a
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-08-20 13:33:27 -0700

    Created Geolocation functionality
    - Successfully connected to the API and can extract the needed data

--- src/App.js
+++ src/App.js
@@ -4,15 +4,40 @@ import MapComponent from './components/MapComponent';
 import "./css/App.css";
 
 const App = () => {
-  let [latLng, setLatLng] = useState([45.5152, -122.6784])
+  let [latLng, setLatLng] = useState([45.5152, -122.6784]);
+  let [ip, setIp] = useState("2601:1c2:1");
+
+  const geolocate = async () => {
+    const APIKey = "66d2e4194749435fb4d137f7e0bd1d90";
+
+    if (ip) {
+      console.log("requesting data...")
+      let response = await fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=${APIKey}&ip=${ip}`);
+
+      if (response.status === 200) {
+        let data = await response.json();
+        console.log(data);
+        setLatLng([data.latitude, data.longitude])
+      } else {
+        console.error("That IP address is invalid or not in the database")
+      }
+
+    } else {
+      console.error("You must provide a vaild IP address")
+    }
+  }
+
   return (
-    <div>
+    <div className='app'>
       <h1>Hi there :)</h1>
       <button onClick={() => { setLatLng([0, 0]) }}>
         Test Button
       </button>
       <button onClick={() => { setLatLng([45.5152, -122.6784]) }}>
         Back to home
+      </button>
+      <button onClick={geolocate}>
+        Geolocate
       </button>
       <MapContainer className="map" center={[45.5152, -122.6784]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
